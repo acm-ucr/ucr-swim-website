928e9b10521542d5c23c250fa15e4cefc8855c72
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-04-07 21:26:45 -0700

    made hovering changes to SwimOutlet

--- src/components/home/SwimOutlet.jsx
+++ src/components/home/SwimOutlet.jsx
@@ -12,7 +12,7 @@ const SwimOutlet = () => {
       <div className="bg-swim-blue-100 absolute w-full bottom-[20%] h-[30vh]" />
 
       <div className="absolute -rotate-90 -left-[10%] bottom-[30%]">
-        <p className="text-swim-blue-300 text-7xl ">Arena Storefront</p>
+        <p className="text-swim-blue-300 text-7xl">Arena Storefront</p>
         <p className="text-white text-6xl font-bold text-center">
           Arena Storefront
         </p>
@@ -20,34 +20,34 @@ const SwimOutlet = () => {
 
       <Image src={SwimOutletImage} alt="Swim Outlet Logo" />
 
-      <div className="grid grid-cols-3 gap-10 mt-10 items-center z-10 pl-[10%] ">
-        <Link href="https://www.swimoutlet.com/">
+      <div className="grid grid-cols-3 gap-10 mt-10 items-center z-10 pl-[10%]">
+        <Link href="https://www.swimoutlet.com/" target="_blank">
           <Image
             src={BathingSuitImage}
             alt="Bathing Suit Image"
-            className="hover:opacity-90"
+            className="hover:opacity-90 transition-opacity ease-in-out"
           />
         </Link>
-        <Link href="https://www.swimoutlet.com/">
+        <Link href="https://www.swimoutlet.com/" target="_blank">
           <Image
             src={JacketImage}
             alt="Jacket Image"
-            className="hover:opacity-90"
+            className="hover:opacity-90 transition-opacity ease-in-out"
           />
         </Link>
         <div className=" flex flex-col justify-between h-full">
-          <Link href="https://www.swimoutlet.com/">
+          <Link href="https://www.swimoutlet.com/" target="_blank">
             <Image
               src={GogglesImage}
               alt="Goggles Image"
-              className="hover:opacity-90"
+              className="hover:opacity-90 transition-opacity ease-in-out"
             />
           </Link>
-          <Link href="https://www.swimoutlet.com/">
+          <Link href="https://www.swimoutlet.com/" target="_blank">
             <Image
               src={ShortsImage}
               alt="Shorts Image"
-              className="hover:opacity-90"
+              className="hover:opacity-90 transition-opacity ease-in-out"
             />
           </Link>
         </div>
