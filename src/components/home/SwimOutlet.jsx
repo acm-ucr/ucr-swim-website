c49f0e284c593f328d891e237193f3ce0cd9aaca
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-04-07 17:21:58 -0700

    commit my changes so I can pull

--- src/components/home/SwimOutlet.jsx
+++ src/components/home/SwimOutlet.jsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import BathingSuitImage from "@/public/home/bathingsuit.webp";
+import GogglesImage from "@/public/home/goggles.webp";
+import JacketImage from "@/public/home/jacket.webp";
+import ShortsImage from "@/public/home/shorts.webp";
+import SwimOutletImage from "@/public/home/swimoutlet.webp";
+
 const SwimOutlet = () => {
-  return <div>SwimOutlet</div>;
+  return (
+    <div className="bg-pink-200">
+      <Image src={SwimOutletImage} alt="Swim Outlet Logo" className="mx-auto"/>
+      <div className="bg-swim-blue-100">
+        <div className="flex">
+          <div className="transform -rotate-90 bg-red-200">
+            <p className="text-swim-blue-300 text-6xl text-center">Arena Storefront</p>
+            <p className="text-white text-4xl font-bold text-center">Arena Storefront</p>
+          </div>
+          <Image src={BathingSuitImage} alt="Bathing Suit Image" />
+          <Image src={JacketImage} alt="Jacket Image" />
+          <div className="">
+            <Image src={GogglesImage} alt="Goggles Image" />
+            <Image src={ShortsImage} alt="Shorts Image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default SwimOutlet;
