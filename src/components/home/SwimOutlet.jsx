d3f9870cf774c0e8fe1436df259886281d5261c2
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-04-07 19:55:22 -0700

    added SwimOutlet Component

--- src/components/home/SwimOutlet.jsx
+++ src/components/home/SwimOutlet.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import BathingSuitImage from "@/public/home/bathingsuit.webp";
 import GogglesImage from "@/public/home/goggles.webp";
 import JacketImage from "@/public/home/jacket.webp";
@@ -7,24 +8,52 @@ import SwimOutletImage from "@/public/home/swimoutlet.webp";
 
 const SwimOutlet = () => {
   return (
-    <div className="bg-pink-200">
-      <Image src={SwimOutletImage} alt="Swim Outlet Logo" className="mx-auto"/>
-      <div className="bg-swim-blue-100">
-        <div className="flex">
-          <div className="transform -rotate-90 bg-red-200">
-            <p className="text-swim-blue-300 text-6xl text-center">Arena Storefront</p>
-            <p className="text-white text-4xl font-bold text-center">Arena Storefront</p>
-          </div>
-          <Image src={BathingSuitImage} alt="Bathing Suit Image" />
-          <Image src={JacketImage} alt="Jacket Image" />
-          <div className="">
-            <Image src={GogglesImage} alt="Goggles Image" />
-            <Image src={ShortsImage} alt="Shorts Image" />
-          </div>
+    <div className="flex flex-col items-center w-full relative">
+      <div className="bg-swim-blue-100 absolute w-full bottom-[20%] h-[30vh]" />
+
+      <div className="absolute -rotate-90 -left-[10%] bottom-[30%]">
+        <p className="text-swim-blue-300 text-7xl ">Arena Storefront</p>
+        <p className="text-white text-6xl font-bold text-center">
+          Arena Storefront
+        </p>
+      </div>
+
+      <Image src={SwimOutletImage} alt="Swim Outlet Logo" />
+
+      <div className="grid grid-cols-3 gap-10 mt-10 items-center z-10 pl-[10%] ">
+        <Link href="https://www.swimoutlet.com/">
+          <Image
+            src={BathingSuitImage}
+            alt="Bathing Suit Image"
+            className="hover:opacity-90"
+          />
+        </Link>
+        <Link href="https://www.swimoutlet.com/">
+          <Image
+            src={JacketImage}
+            alt="Jacket Image"
+            className="hover:opacity-90"
+          />
+        </Link>
+        <div className=" flex flex-col justify-between h-full">
+          <Link href="https://www.swimoutlet.com/">
+            <Image
+              src={GogglesImage}
+              alt="Goggles Image"
+              className="hover:opacity-90"
+            />
+          </Link>
+          <Link href="https://www.swimoutlet.com/">
+            <Image
+              src={ShortsImage}
+              alt="Shorts Image"
+              className="hover:opacity-90"
+            />
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default SwimOutlet;
