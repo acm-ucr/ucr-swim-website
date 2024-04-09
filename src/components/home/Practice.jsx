0489c034e32560b344da5e88333e9741968d516a
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-04-09 16:39:57 -0700

    practice component fixed

--- src/components/home/Practice.jsx
+++ src/components/home/Practice.jsx
@@ -4,23 +4,26 @@ import waveBottom from "@/public/waveBottom.webp";
 
 const Practice = () => {
   return (
-    <div className="h-[550px] bg-gradient-to-r from-swim-blue-100 to-swim-blue-300">
-      <div className="container grid grid-cols-2 gap-20">
-        <Image src={practice} alt="practice" className="" />
-        <div className="mt-24 font-urbanist text-white ">
-          <div className="text-8xl mb-10 font-semibold">Practices</div>
-          <div className="container grid grid-cols-2 grid-rows-2 text-3xl">
-            <div className="mb-3">Tuesday and Thursday </div>
-            <div className=" text-swim-yellow font-bold">7:00 pm - 8:20 pm</div>
-            <div>Sunday </div>
-            <div className="text-swim-yellow font-bold">6:00 pm - 7:20 pm</div>
-          </div>
+    <div className="flex w-full bg-gradient-to-r from-swim-blue-100 to-swim-blue-300 text-3xl relative">
+      <Image src={practice} alt="practice" className="w-1/2" />
+
+      <div className="w-1/2 pt-[8%] px-[6%] text-white">
+        <p className="text-8xl mb-5 font-semibold">Practices</p>
+
+        <div className="flex justify-between w-full">
+          <p>Tuesday and Thursday </p>
+          <p className=" text-swim-yellow font-bold mb-5">7:00 pm - 8:20 pm</p>
+        </div>
+
+        <div className="flex justify-between w-full">
+          <p>Sunday </p>
+          <p className="text-swim-yellow font-bold">6:00 pm - 7:20 pm</p>
         </div>
       </div>
       <Image
         src={waveBottom}
         alt="waveBottom"
-        className="relative w-screen h-36 bottom-20"
+        className="absolute w-screen -bottom-14"
       />
     </div>
   );
