d1b1c559d288561aa0de370cf62de1ed6040cc4d
Sherry <shsieh005@gmail.com>
2024-04-09 23:19:50 -0400

    Landing fix

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -7,19 +7,17 @@ const Landing = () => {
   return (
     <div className="w-screen relative -translate-y-5">
       <Image src={image1} alt="Background image 1" className="w-screen" />
-      <div className="flex justify-center w-[50%] h-full bg-swim-blue-200/60 top-[0%] absolute">
-        <div className="flex flex-col absolute top-[40%] left-[7%]">
-          <p className="font-bold text-white text-6xl font-urbanist mb-4">
-            UCR Club Swim
-          </p>
-          <p className="font-semibold text-white text-3xl font-urbanist ml-10 mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod
-          </p>
-          <Button link="/join" text="Join" />
-        </div>
+      <div className="flex justify-center flex-col w-1/2 h-full bg-swim-blue-200/60 top-0 absolute pl-12">
+        <p className="font-bold text-white text-6xl font-urbanist mb-4">
+          UCR Club Swim
+        </p>
+        <p className="font-semibold text-white text-3xl font-urbanist ml-10 mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod
+        </p>
+        <Button link="/join" text="Join" />
       </div>
-      <div className="w-screen h-full bg-swim-blue-200/15 top-[0%] absolute" />
+      <div className="w-screen h-full bg-swim-blue-200/15 top-0 absolute" />
       <Image
         src={image2}
         alt="Background image 2"
