4d99eac296699c139d6eef128ccc05ee36e82b88
jinnoh <jinnoh42@gmail.com>
2024-04-11 13:10:10 -0700

    trying to commit changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,15 @@
-const Button = () => {
+import Link from "next/link";
+
+const Button = ({ link, target }) => {
   return (
-    <div className="">
-      <p>Button</p>
-    </div>
+    <Link
+      className="h-20 w-13 text-4xl text-swim-black bg-swim-yellow px-10 py-5 font-bold text-xl text-center hover:opacity-50 active:opacity-40 z-10"
+      href="https://acm.cs.ucr.edu/"
+      target="_blank"
+    >
+      <p className="align-middle"> Member Info Form</p>
+    </Link>
+    //
   );
 };
 
