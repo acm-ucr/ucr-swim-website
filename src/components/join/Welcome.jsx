2719c73843a85caa87f0e8dc42384da248290451
jisubang1001 <bangjisu1001@gmail.com>
2024-04-10 20:34:31 -0400

    Implement Welcome.jsx

--- src/components/join/Welcome.jsx
+++ src/components/join/Welcome.jsx
@@ -1,7 +1,15 @@
 const Welcome = () => {
   return (
-    <div className="">
-      <p>Welcome</p>
+    <div className="flex flex-col items-center bg-swim-blue-200 w-screen p-10">
+      <p className="text-swim-yellow font-urbanist text-3xl font-semibold">
+        WELCOME
+      </p>
+      <p className="font-urbanist text-white">
+        {" "}
+        Now you&quot;re officially a part of Swim Club, you&quot;re able to swim
+        at our practices! However, be sure to bring your R&quot;Card to every
+        practice. You won&quot;t be able to swim without it!
+      </p>
     </div>
   );
 };
