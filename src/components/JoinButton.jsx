c7f90a159bf05971b76f4830611a749eb09390b9
Kaushik Vada <58670585+kaushikvada3@users.noreply.github.com>
2024-04-12 13:50:04 -0700

    update

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const JoinButton = () => {
   return (
-    <a
+    <Link
       href="/join" // Link to the join page
       className="bg-yellow-300 text-black py-2.5 px-5 border-none rounded-md cursor-pointer no-underline hover:opacity-70"
     >
       Join
-    </a>
+    </Link>
   );
 };
 
