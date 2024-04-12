b18ef3c78e9db45064b9cc200acedf873957cb5e
user <user@MacBook-Pro-7.junior.emma.troy.ny.us>
2024-04-11 22:52:06 -0400

    complete changes

--- src/components/home/Connected.jsx
+++ src/components/home/Connected.jsx
@@ -1,12 +1,8 @@
 const Connected = () => {
   return (
     <div className="flex justify-center items-center flex-col">
-      <p className="text-swim-blue-300 flex justify-center text-7xl pt-5">
-        Stay Connected
-      </p>
-      <p className="text-swim-yellow flex justify-center text-2xl">
-        Lorem ipsum dolor sit amet
-      </p>
+      <p className="text-swim-blue-300 text-7xl pt-5">Stay Connected</p>
+      <p className="text-swim-yellow text-2xl">Lorem ipsum dolor sit amet</p>
     </div>
   );
 };
