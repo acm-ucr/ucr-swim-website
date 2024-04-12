1c1240c59ca3a289c82e5a39083e2c50fdc0eb95
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-04-12 00:11:12 -0700

    format fix

--- src/components/join/Welcome.jsx
+++ src/components/join/Welcome.jsx
@@ -1,9 +1,7 @@
 const Welcome = () => {
   return (
     <div className="flex flex-col items-center bg-swim-blue-200 w-screen p-10">
-      <p className="text-swim-yellow text-3xl font-semibold">
-        WELCOME
-      </p>
+      <p className="text-swim-yellow text-3xl font-semibold">WELCOME</p>
       <p className="text-white">
         Now you&apos;re officially a part of Swim Club, you&apos;re able to swim
         at our practices! However, be sure to bring your R&apos;Card to every
