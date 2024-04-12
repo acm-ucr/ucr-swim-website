09acc039c0a6e818fb9761ae388df0b26ff3da8f
jinnoh <jinnoh42@gmail.com>
2024-04-11 19:11:33 -0700

    Changed with to go based off text size, and added link, target and text props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,14 @@
 import Link from "next/link";
 
-const Button = ({ link, target }) => {
+const Button = ({ link, text, target }) => {
   return (
     <Link
-      className="h-20 w-13 text-4xl text-swim-black bg-swim-yellow px-10 py-5 font-bold text-xl text-center hover:opacity-50 active:opacity-40 z-10"
-      href="https://acm.cs.ucr.edu/"
-      target="_blank"
+      className="h-15 w-fit text-4xl text-swim-black bg-swim-yellow px-10 py-5 font-bold text-xl text-center hover:opacity-50 active:opacity-40 z-10"
+      href={link}
+      target={target}
     >
-      <p className="align-middle"> Member Info Form</p>
+      {text}
     </Link>
-    //
   );
 };
 
