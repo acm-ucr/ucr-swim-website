fd3b5ad6a5a330e4861460210805ba045385086b
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2024-04-11 22:24:57 -0700

    Update src/components/join/Welcome.jsx

--- src/components/join/Welcome.jsx
+++ src/components/join/Welcome.jsx
@@ -4,7 +4,7 @@ const Welcome = () => {
       <p className="text-swim-yellow font-urbanist text-3xl font-semibold">
         WELCOME
       </p>
-      <p className="font-urbanist text-white">
+      <p className="text-white">
         Now you&apos;re officially a part of Swim Club, you&apos;re able to swim
         at our practices! However, be sure to bring your R&apos;Card to every
         practice. You won&apos;t be able to swim without it!
