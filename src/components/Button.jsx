c175e833ffb3add0fb7ba4bf6b826744696401da
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2024-04-11 19:20:11 -0700

    Update src/components/Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Button = ({ link, text, target }) => {
   return (
     <Link
-      className="h-15 w-fit text-4xl text-swim-black bg-swim-yellow px-10 py-5 font-bold text-xl text-center hover:opacity-50 active:opacity-40 z-10"
+      className="h-15 w-fit text-2xl text-swim-black bg-swim-yellow px-10 py-3 font-bold text-center hover:opacity-50 active:opacity-40 z-10"
       href={link}
       target={target}
     >
