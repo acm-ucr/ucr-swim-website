a7c24ab98388165683cdc47d83d03dac17e2c734
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-04-14 14:00:07 -0700

    added Picture component

--- src/components/gallery/Picture.jsx
+++ src/components/gallery/Picture.jsx
@@ -1,8 +1,22 @@
-const Picture = () => {
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Picture = ({ text, link, image }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="">
-      <p>Picture</p>
-    </div>
+    <Link href={link} target="_blank">
+      <div className="relative flex justify-center items-center">
+        {isHovered && <p className="absolute">{text}</p>}
+        <Image
+          src={image}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        />
+      </div>
+    </Link>
   );
 };
 
