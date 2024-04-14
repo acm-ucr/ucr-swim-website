e1538e7af170413168595cb8fc124fa250478376
Sherry <shsieh005@gmail.com>
2024-04-14 15:51:09 -0400

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,44 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import logo from "@/public/logo.webp";
+import { items } from "@/data/Navigation";
+
 const NavBar = () => {
+  const [selected, setSelected] = useState("");
   return (
-    <div className="fixed z-10 w-full bg-white bg-opacity-50">
-      <p>NavBar</p>
+    <div className="px-8 fixed z-30 h-[10vh] bg-white w-screen flex justify-between items-center text-xl">
+      <Link
+        onClick={() => {
+          setSelected("");
+        }}
+        href="/"
+      >
+        <Image
+          src={logo}
+          alt="Logo"
+          className="h-[8vh] w-[10vh] hover:opacity-60"
+        />
+      </Link>
+      <div className="flex justify-between w-1/2">
+        {items.map((item, index) => (
+          <Link
+            href={item.link}
+            key={index}
+            onClick={() => {
+              setSelected(item.name);
+            }}
+            className={`hover:text-swim-blue-300 duration-300 border-solid ${
+              selected === item.name
+                ? "border-b-2 border-swim-yellow text-swim-blue-300"
+                : "text-black"
+            }`}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
