c6db91894560580ffb3d55f7d275954917ecd4a0
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-04-15 22:37:01 -0700

    made changes to text when hovering

--- src/components/gallery/Picture.jsx
+++ src/components/gallery/Picture.jsx
@@ -1,20 +1,15 @@
-"use client";
-import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Picture = ({ text, link, image }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
     <Link href={link} target="_blank">
-      <div className="relative flex justify-center items-center">
-        {isHovered && <p className="absolute">{text}</p>}
-        <Image
-          src={image}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        />
+      <div className="group relative flex justify-center items-center">
+        <p className="absolute opacity-0 group-hover:opacity-100 text-white z-10 group-hover:font-semibold group-hover:text-4xl">
+          {text}
+        </p>
+        <div className="opacity-0 group-hover:opacity-50 bg-gray-900 w-full h-full absolute group-hover:duration-150"></div>
+        <Image src={image} />
       </div>
     </Link>
   );
