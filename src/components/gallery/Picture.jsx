8f3383797ccb6a026342498638e33b64d1b93f45
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2024-04-17 14:03:10 -0700

    Update src/components/gallery/Picture.jsx

--- src/components/gallery/Picture.jsx
+++ src/components/gallery/Picture.jsx
@@ -8,7 +8,7 @@ const Picture = ({ text, link, image }) => {
         <p className="absolute opacity-0 group-hover:opacity-100 text-white z-10 font-semibold text-4xl duration-150">
           {text}
         </p>
-        <div className="opacity-0 group-hover:opacity-50 bg-gray-900 w-full h-full absolute duration-150"></div>
+        <div className="opacity-0 group-hover:opacity-50 bg-gray-900 w-full h-full absolute duration-150" />
         <Image src={image} alt="gallery image" />
       </div>
     </Link>
