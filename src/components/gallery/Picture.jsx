c7f35f7b0af45f7ecd98b2da8a1349eef2d9ce98
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-04-16 20:04:57 -0700

    made minor changes to coding style

--- src/components/gallery/Picture.jsx
+++ src/components/gallery/Picture.jsx
@@ -5,10 +5,10 @@ const Picture = ({ text, link, image }) => {
   return (
     <Link href={link} target="_blank">
       <div className="group relative flex justify-center items-center">
-        <p className="absolute opacity-0 group-hover:opacity-100 text-white z-10 group-hover:font-semibold group-hover:text-4xl">
+        <p className="absolute opacity-0 group-hover:opacity-100 text-white z-10 font-semibold text-4xl duration-150">
           {text}
         </p>
-        <div className="opacity-0 group-hover:opacity-50 bg-gray-900 w-full h-full absolute group-hover:duration-150"></div>
+        <div className="opacity-0 group-hover:opacity-50 bg-gray-900 w-full h-full absolute duration-150"></div>
         <Image src={image} alt="gallery image" />
       </div>
     </Link>
