213299e0a2b5b3c769263edf4eff03c8eefa1921
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2024-04-17 23:30:35 -0700

    Update src/components/calendar/CustomEvent.jsx

--- src/components/calendar/CustomEvent.jsx
+++ src/components/calendar/CustomEvent.jsx
@@ -1,7 +1,7 @@
 const CustomEvent = (event) => {
   return (
     <div className="bg-swim-blue-200 text-center text-white text-xl h-7 flex items-center justify-center">
-      <p className="whitespace-nowrap m-0"> {event.title} </p>
+      <p className="whitespace-nowrap m-0">{event.title}</p>
     </div>
   );
 };
