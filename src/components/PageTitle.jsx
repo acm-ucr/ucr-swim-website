3ab0290692ced273f54e09b27d6939fc909f5ce1
Jiahan Li <jcl@JiahandeAir.sophmore.emma.troy.ny.us>
2024-04-17 20:50:40 -0400

    PageTitle

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,7 +1,22 @@
-const PageTitle = () => {
+import Image from "next/image";
+import wave from "@/public/waveBottom.webp";
+
+const PageTitle = ({ image, title }) => {
   return (
-    <div className="">
-      <p>PageTitle</p>
+    <div className="w-full relative">
+      <Image
+        src={image}
+        alt="background image"
+        className="w-screen opacity-80"
+      />
+      <p className="absolute top-[0%] pl-[10%] font-bold text-5xl text-white border-swim-yellow border-r-[20px] pr-[10%] py-[15%] bg-swim-blue-200/70">
+        {title}
+      </p>
+      <Image
+        src={wave}
+        alt="wave bottom image"
+        className="absolute top-[80%] transform -scale-x-100 w-screen"
+      />
     </div>
   );
 };
