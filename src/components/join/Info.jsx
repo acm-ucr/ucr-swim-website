97df7d6212cbca8e4de4f87c775daf7492bfa507
user <user@MacBook-Pro-7.junior.emma.troy.ny.us>
2024-04-18 23:06:49 -0400

    info

--- src/components/join/Info.jsx
+++ src/components/join/Info.jsx
@@ -1,7 +1,20 @@
+import InfoCard from "./InfoCard";
+
 const Info = () => {
   return (
-    <div className="">
-      <p>Info</p>
+    <div className="w-full l-100 flex">
+      <div className="flex flex-col ml-28 mt-24">
+        <p className="text-swim-yellow md:text-2xl">Lorem ipsum dolor sit</p>
+        <p className="text-swim-blue-400 md:text-4xl ">
+          How to keep up with <br />
+          information, <br />
+          competitions, and <br />
+          socials:
+        </p>
+      </div>
+      <div className="ml-auto">
+        <InfoCard />
+      </div>
     </div>
   );
 };
