86c6e968f48f9c8daa5e0379d4e4909d23c66c78
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-04-18 21:21:48 -0700

    updated connected

---
+++ src/data/iconLinks.js
@@ -0,0 +1,35 @@
+import {
+  RiInstagramFill,
+  RiDiscordFill,
+  RiShoppingBagLine,
+  RiLinksFill,
+} from "react-icons/ri";
+import { BiSwim } from "react-icons/bi";
+
+export const iconLinks = [
+  {
+    icon: <RiInstagramFill />,
+    label: "Instagram",
+    link: "https://www.instagram.com/ucrclubswim?igsh=MzRlODBiNWFlZA==",
+  },
+  {
+    icon: <RiDiscordFill />,
+    label: "Discord",
+    link: "https://highlanderlink.ucr.edu/organization/swimclubatucr",
+  },
+  {
+    icon: <RiShoppingBagLine />,
+    label: "Swim Outlet",
+    link: "https://google.com",
+  },
+  {
+    icon: <RiLinksFill />,
+    label: "Highlander Link",
+    link: "https://highlanderlink.ucr.edu/organization/swimclubatucr",
+  },
+  {
+    icon: <BiSwim />,
+    label: "Do Sports",
+    link: "https://recreation.ucr.edu/competitive-sports/do-sports-easy",
+  },
+];
