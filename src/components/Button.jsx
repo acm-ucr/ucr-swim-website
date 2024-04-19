2c834b44b38b872d6bca1c4ebb919656817a4d40
Motunrayooluwa Ogunwusi <motunrayooluwaogunwusi@MotunraluwasAir.junior.emma.troy.ny.us>
2024-04-19 10:33:32 -0400

    i fixed my changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-const Button = ({ link, text, target, color }) => {
+const Button = ({ link, text, target, color = "text-black" }) => {
   return (
     <Link
-      className={` ${color} h-15 w-fit text-2xl text-black bg-swim-yellow px-10 py-3 font-bold text-center hover:opacity-50 active:opacity-40 z-10`}
+      className={` ${color} h-15 w-fit text-2xl bg-swim-yellow px-10 py-3 font-bold text-center hover:opacity-50 active:opacity-40 z-10`}
       href={link}
       target={target}
     >
