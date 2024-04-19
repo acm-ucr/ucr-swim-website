89f73cca3412c142228c943f4f9207c8d4f4ef18
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-04-18 21:24:54 -0700

    updated connected

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Board = ({ picture, name, position }) => {
   return (
     <div className="relative">
-      <Image src={picture} />
+      <Image src={picture} alt="picture" />
       <div className="absolute -bottom-5 w-full">
         <p className="bg-swim-blue-100 text-3xl text-center font-urbanist w-3/4 p-2">
           {position}
