d7976460b88b1e619caf597bb12a94ce1023901d
Jiahan Li <jcl@JiahandeAir.sophmore.emma.troy.ny.us>
2024-04-18 23:02:26 -0400

    PageTitle

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -9,13 +9,13 @@ const PageTitle = ({ image, title }) => {
         alt="background image"
         className="w-screen opacity-80"
       />
-      <p className="absolute top-[0%] pl-[10%] font-bold text-5xl text-white border-swim-yellow border-r-[20px] pr-[10%] py-[15%] bg-swim-blue-200/70">
+      <p className="flex items-center justify-center absolute top-0 font-bold text-5xl text-white border-swim-yellow border-r-[20px] w-1/4 h-full bg-swim-blue-200/70">
         {title}
       </p>
       <Image
         src={wave}
         alt="wave bottom image"
-        className="absolute top-[80%] transform -scale-x-100 w-screen"
+        className="absolute bottom-0 transform -scale-x-100 w-screen"
       />
     </div>
   );
