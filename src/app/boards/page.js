c1bb84a4482f098943c7f5b16ad62dfd67ee07f3
jisubang1001 <bangjisu1001@gmail.com>
2024-04-18 21:43:59 -0400

    Implement Board

--- src/app/boards/page.js
+++ src/app/boards/page.js
@@ -1,6 +1,10 @@
+import Board from "@/components/board/Board";
+import imageexample from "@/public/board/temp.webp";
+
 const Boards = () => {
   return (
     <div className="w-full flex flex-col items-center">
+      <Board picture={imageexample} position="Position Title" name="Name" />
       <p>Boards</p>
     </div>
   );
