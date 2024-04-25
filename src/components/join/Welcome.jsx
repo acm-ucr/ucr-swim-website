879adc199876c964cda2f4c512dc4c00f1e65bcd
jinnoh <jinnoh42@gmail.com>
2024-04-24 17:41:07 -0700

    closes #79

--- src/components/join/Welcome.jsx
+++ src/components/join/Welcome.jsx
@@ -1,8 +1,10 @@
 const Welcome = () => {
   return (
-    <div className="flex flex-col items-center bg-swim-blue-200 w-screen py-[3%]">
-      <p className="text-swim-yellow text-3xl font-semibold mb-2">WELCOME</p>
-      <p className="text-white w-3/5 text-center">
+    <div className="flex flex-row md:flex-col items-center bg-swim-blue-200 w-screen py-[10%] md:py-[3%] px-[5%] md:px-[0%] gap-x-[5%] md:gap-x-[0%]">
+      <p className="text-swim-yellow text-3xl md:text-3xl font-semibold mb-2">
+        WELCOME
+      </p>
+      <p className="text-white w-3/5 text-center text-xs md:text-base">
         Now you&apos;re officially a part of Swim Club, you&apos;re able to swim
         at our practices! However, be sure to bring your R&apos;Card to every
         practice. You won&apos;t be able to swim without it!
