cf8a96538eab283a09c406e98dde84466515c6cb
Motunrayooluwa Ogunwusi <motunrayooluwaogunwusi@Motunrayooluwas-MacBook-Air.local>
2024-05-02 00:01:24 -0400

    i made changes to practice.jsx

--- src/components/home/Practice.jsx
+++ src/components/home/Practice.jsx
@@ -10,12 +10,12 @@ const Practice = () => {
       <div className="w-1/2 pt-[8%] px-[6%] text-white">
         <p className="md:text-7xl 2xl:text-8xl mb-5 font-semibold">Practices</p>
 
-        <div className="flex justify-between md:w-full">
+        <div className="flex justify-between md:text-xl 2xl:text-xl mb-5 md:w-full">
           <p>Tuesday and Thursday </p>
-          <p className=" text-swim-yellow font-bold mb-5">7:00 pm - 8:20 pm</p>
+          <p className=" text-swim-yellow font-bold ">7:00 pm - 8:20 pm</p>
         </div>
 
-        <div className="flex justify-between w-full">
+        <div className="flex justify-between md:text-xl 2xl:text-xl mb-8 md:w-full">
           <p>Sunday </p>
           <p className="text-swim-yellow font-bold">6:00 pm - 7:20 pm</p>
         </div>
