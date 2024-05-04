9fb3b7f70c3aa953ac3d4b23d2c8633cc2587204
Shing Hung <yhung022@ucr.edu>
2024-05-03 17:10:54 -0700

    add animation

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,13 +1,29 @@
+"use client";
 import Image from "next/image";
-
+import useView from "@/components/useView";
 const Board = ({ picture, name, position }) => {
+  const [inView, ref] = useView();
+
   return (
-    <div className="w-52 justify-self-center">
+    <div className={`w-52 justify-self-center`}>
       <Image src={picture} alt="picture" className="aspect-square" />
-      <div className="w-full -translate-y-5 text-nowrap text-xl text-center">
-        <p className="bg-swim-blue-100 w-4/5 p-1 ">{position}</p>
+      <div
+        ref={ref}
+        className="w-full -translate-y-5 text-nowrap text-xl text-center"
+      >
+        <p
+          className={`${
+            inView && "animate-fade-right"
+          } bg-swim-blue-100 w-4/5 p-1 `}
+        >
+          {position}
+        </p>
         <div className="w-full justify-end flex">
-          <p className="bg-swim-blue-200 text-white p-1 -translate-y-1 w-4/5">
+          <p
+            className={`${
+              inView && "animate-fade-left"
+            } bg-swim-blue-200 text-white p-1 -translate-y-1 w-4/5`}
+          >
             {name}
           </p>
         </div>
