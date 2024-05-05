64a662377482f7b49b5f2aef24164a9a03c6088d
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-05-05 11:34:17 -0700

    added gallery animation

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -3,8 +3,10 @@ import { PICTURES } from "@/data/pictures.js";
 import PageTitle from "@/components/PageTitle";
 import gallery from "@/public/gallery/gallery.webp";
 import Picture from "@/components/gallery/Picture.jsx";
+import useView from "@/components/useView.jsx";
 
 const Gallery = () => {
+  const [inView, ref] = useView();
   return (
     <div className="w-full flex flex-col items-center">
       <PageTitle title="Gallery" image={gallery} />
@@ -13,14 +15,21 @@ const Gallery = () => {
         Check Out Our Photos
       </p>
 
-      <div className="lg:grid lg:grid-cols-2 w-4/5">
+      <div ref={ref} className="lg:grid lg:grid-cols-2 w-4/5">
         {PICTURES.map((picture, index) => (
-          <Picture
+          <div
             key={index}
-            text={picture.text}
-            link={picture.link}
-            image={picture.image}
-          />
+            className={`${
+              inView &&
+              `animate-flip-up animate-once animate-ease-out animate-duration-[4000ms]`
+            }`}
+          >
+            <Picture
+              text={picture.text}
+              link={picture.link}
+              image={picture.image}
+            />
+          </div>
         ))}
       </div>
     </div>
