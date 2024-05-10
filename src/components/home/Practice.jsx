450679b97d72f5a8639a87d1dab92e171bad2081
hhannahhwang <hhannah.k.hwang@gmail.com>
2024-05-09 17:44:41 -0700

    practice Animated

--- src/components/home/Practice.jsx
+++ src/components/home/Practice.jsx
@@ -1,17 +1,28 @@
+"use client";
 import Image from "next/image";
 import practice from "@/public/home/practice.webp";
 import waveBottom from "@/public/waveBottom.webp";
+import useView from "@/components/useView";
 
 const Practice = () => {
+  const [inView, ref] = useView();
+
   return (
-    <div className="flex w-full bg-gradient-to-r from-swim-blue-100 to-swim-blue-300 text-sm md:text-2xl 2xl:text-3xl relative mb-[8%]">
+    <div
+      ref={ref}
+      className="flex w-full bg-gradient-to-r from-swim-blue-100 to-swim-blue-300 text-sm md:text-2xl 2xl:text-3xl relative mb-[8%]"
+    >
       <Image
         src={practice}
         alt="practice"
         className="md:w-1/2 w-[55%] object-cover"
       />
 
-      <div className="w-1/2 md:pt-[8%] md:px-[6%] pt-4 px-3 pb-7 md:pb-0 text-white">
+      <div
+        className={`${
+          inView && "animate-fade-up animate-delay-150"
+        } w-1/2 md:pt-[8%] md:px-[6%] pt-4 px-3 pb-7 md:pb-0 text-white`}
+      >
         <p className="text-2xl md:text-6xl 2xl:text-8xl md:mb-5 mb-2 font-semibold">
           Practices
         </p>
@@ -24,8 +35,8 @@ const Practice = () => {
         </div>
 
         <div className="md:flex md:justify-between w-full">
-          <p>Sunday </p>
-          <p className="text-swim-yellow font-bold">6:00 pm - 7:20 pm</p>
+          <p> Sunday </p>
+          <p className=" text-swim-yellow font-bold">6:00 pm - 7:20 pm</p>
         </div>
       </div>
       <Image
