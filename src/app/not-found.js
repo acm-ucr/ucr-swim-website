779f4266bd7a42e2106842b466fcdddc9f8728b9
Sherry <shsieh005@gmail.com>
2024-05-10 20:04:02 -0400

    404 Error Update

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,8 +5,8 @@ import Button from "@/components/Button";
 const NotFound = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <p className="text-4xl font-bold"> - Oops!</p>
-      <Image src={fish} alt="Fish image" className="w-2/5 m-7"></Image>
+      <p className="text-4xl font-bold">Oops!</p>
+      <Image src={fish} alt="Fish image" className="w-2/5 m-7" />
       <p className="text-8xl font-bold">404</p>
       <p>Page Not Found</p>
       <p>Are you stuck? Just keep swimming!</p>
