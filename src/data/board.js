ae3aa987cacadf8527b1adddead03643512a4e3a
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-05-18 18:54:10 -0700

    fixed board page

---
+++ src/data/board.js
@@ -0,0 +1,47 @@
+export const BOARDS = [
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+  {
+    name: "Evan Littlecat",
+    position: "President",
+    img: "/board/temp.webp",
+  },
+];
