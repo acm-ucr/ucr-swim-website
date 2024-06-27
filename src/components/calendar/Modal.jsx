49ef30baf2a2566fed16270dca3e26fcf1977e22
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-06-27 14:11:41 -0700

    added board + fixed calendar

--- src/components/calendar/Modal.jsx
+++ src/components/calendar/Modal.jsx
@@ -2,15 +2,15 @@ import { AiOutlinePlus } from "react-icons/ai";
 
 const Modal = ({ events, setEvents }) => {
   return (
-    <div className="text-white fixed left-1/2 top[56%] -translate-x-1/4 -translate-y-1/2 w-[30vw] bg-swim-blue-200 z-10 drop-shadow-sm">
+    <div className="text-white fixed left-1/2 top-[50%] -translate-x-1/2 -translate-y-1/2 w-[30vw] bg-swim-blue-200 z-10 drop-shadow-sm p-2">
       <div className="flex justify-between items-center">
-        <p className="py-2 px-3 text-lg font-bold">{events.summary}</p>
+        <p className="py-2 px-3 text-4xl font-bold">{events.summary}</p>
         <AiOutlinePlus
           onClick={() => setEvents(null)}
           className="rotate-45 p-0 hover:scale-110 duration-300 hover:cursor-pointer text-2xl 2xl:text-3xl m-3"
         />
       </div>
-      <div className="p-3">
+      <div className="p-3 text-xl">
         <p className="m-0 pl-2 font-bold">{events.location}</p>
         <p className="m-0 pl-2 italic">{events.start.toString()}</p>
         <p className="m-0 pl-2 italic">{events.end.toString()}</p>
